52721fb5446d34be8a9ee993fdc594480ee6c41d
Imransk7768 <imransk7768@gmail.com>
2022-11-14 08:21:43 +0530

    implementing routing & flexLayout for register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './Components/registration/registration.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:'register',component:RegistrationComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]

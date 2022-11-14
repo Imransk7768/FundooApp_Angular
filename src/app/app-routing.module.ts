1669383ab984c9295c315391638ef150ef7a14c0
Imransk7768 <imransk7768@gmail.com>
2022-11-14 09:20:14 +0530

    implementing routing & flexLayout for ForgorPassword page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './Components/registration/registration.component';
 import { LoginComponent } from './Components/login/login.component';
 import { ForgetPasswordComponent } from './Components/forget-password/forget-password.component';
+import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
+
 const routes: Routes = [
   {path:'register',component:RegistrationComponent},
   {path:'login',component:LoginComponent},
-  {path:'forgetpassword',component:ForgetPasswordComponent}
+  {path:'forgetpassword',component:ForgetPasswordComponent},
+  {path:'resetpassword', component:ResetPasswordComponent}
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]

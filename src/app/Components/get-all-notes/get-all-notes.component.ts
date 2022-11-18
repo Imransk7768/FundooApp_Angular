79bb362bd6e2beaea78b0e6b21fde75a9213f69d
Imransk7768 <imransk7768@gmail.com>
2022-11-18 20:43:49 +0530

    implement get all notes

---
+++ src/app/Components/get-all-notes/get-all-notes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NoteService } from 'src/app/Services/note.service';
+
+@Component({
+  selector: 'app-get-all-notes',
+  templateUrl: './get-all-notes.component.html',
+  styleUrls: ['./get-all-notes.component.scss']
+})
+export class GetAllNotesComponent implements OnInit {
+  noteArray:any;
+  constructor(private notesService:NoteService) { }
+
+  ngOnInit(): void {
+  //   this.getAllNotes();
+  // }
+  // getAllNotes(){
+  //   this.notesService.GetNotes().subscribe((request:any)=>{
+  //     console.log("request data",request);
+  //     this.noteArray=request.data.data;
+  //     console.log("noteArray",this.noteArray)
+  //   })
+  }
+
+}

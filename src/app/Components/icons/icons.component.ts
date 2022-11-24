24987d7c5ed4f72f077338abe26744f07e99b874
Imransk7768 <imransk7768@gmail.com>
2022-11-24 17:36:39 +0530

    implemented trash and archieve for notes

--- src/app/Components/icons/icons.component.ts
+++ src/app/Components/icons/icons.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NoteService } from 'src/app/Services/noteServices/note.service';
 
 @Component({
   selector: 'app-icons',
@@ -6,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./icons.component.scss']
 })
 export class IconsComponent implements OnInit {
+@Input() notesCard:any;
 
-  constructor() { }
+data:any;
+noteId:any;
+//isDelete:boolean=false;
+isArchive:boolean=false;
+
+  constructor(public notes:NoteService) { }
 
   ngOnInit(): void {
+  }
+  trash(){
+    let data={
+      noteId:[this.notesCard.noteId],
+      //isDelete:true,
+    }
+    console.log(data);
+    this.notes.TrashNote(data).subscribe((response:any)=>{
+      console.log(response);
+    })
+  }
+  archeive(){
+    let data={
+      noteId:[this.notesCard.noteId],
+    }
+    console.log(data);
+    this.notes.ArchiveNote(data).subscribe((response:any)=>{
+      console.log(response);
+    })
   }
 }

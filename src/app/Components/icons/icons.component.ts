fb551027352d13cc514b7a10e02fe95ba98e48b1
Imransk7768 <imransk7768@gmail.com>
2022-11-27 17:27:32 +0530

    implemented trash and archieve for notes

--- src/app/Components/icons/icons.component.ts
+++ src/app/Components/icons/icons.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NoteService } from 'src/app/Services/noteServices/note.service';
 
 @Component({
@@ -8,11 +8,28 @@ import { NoteService } from 'src/app/Services/noteServices/note.service';
 })
 export class IconsComponent implements OnInit {
 @Input() notesCard:any;
+@Output() displaytoIcons = new EventEmitter<string>()
+
 
 data:any;
 noteId:any;
 //isDelete:boolean=false;
 isArchive:boolean=false;
+
+colors:Array<any> = [
+{code:'#fff',name:'white'},
+{code:'#f28b82',name:'red'},
+{code:'#fbbc04',name:'orange'},
+{code:'#fff475',name:'yellow'},
+{code:'#ccff90',name:'green'},
+{code:'#a7ffeb',name:'teal'},
+{code:'#cbf0f8',name:'blue'},
+{code:'#aecbfa',name:'darkblue'},
+{code:'#d7aefb',name:'purple'},
+{code:'#fdcfe8',name:'pink'},
+{code:'#e6c9a8',name:'brown'},
+{code:'#e8eaed',name:'gray'}
+];
 
   constructor(public notes:NoteService) { }
 
@@ -25,7 +42,8 @@ isArchive:boolean=false;
     }
     console.log(data);
     this.notes.TrashNote(data).subscribe((response:any)=>{
-      console.log(response);
+     console.log(response);
+     this.displaytoIcons.emit(response);
     })
   }
   archeive(){
@@ -35,6 +53,20 @@ isArchive:boolean=false;
     console.log(data);
     this.notes.ArchiveNote(data).subscribe((response:any)=>{
       console.log(response);
+      this.displaytoIcons.emit(response);
+    })
+  }
+  backColor(color:any){
+    this.notesCard.color=color;
+    let data={
+      Color:color,
+      noteId:[this.notesCard.noteId],
+    }
+    console.log(data);
+    this.notes.BackColor(data).subscribe((response:any)=>{
+    console.log('color',color);
+    console.log(response);
+    console.log(this.notesCard);
     })
   }
 }

553459c780dba37e2b4ebc7e83ddb9c73223b613
Imransk7768 <imransk7768@gmail.com>
2022-12-01 09:18:42 +0530

    implemented trash and archieve for notes

--- src/app/Components/icons/icons.component.ts
+++ src/app/Components/icons/icons.component.ts
@@ -1,5 +1,10 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 import { NoteService } from 'src/app/Services/noteServices/note.service';
+import { ArchiveComponent } from '../archive/archive.component';
+import { TrashComponent } from '../trash/trash.component';
 
 @Component({
   selector: 'app-icons',
@@ -16,6 +21,9 @@ noteId:any;
 //isDelete:boolean=false;
 isArchive:boolean=false;
 
+isTrash: boolean = false;
+isArcheive: boolean = false;
+
 colors:Array<any> = [
 {code:'#fff',name:'white'},
 {code:'#f28b82',name:'red'},
@@ -31,9 +39,16 @@ colors:Array<any> = [
 {code:'#e8eaed',name:'gray'}
 ];
 
-  constructor(public notes:NoteService) { }
+  constructor(public notes:NoteService, private snackBar:MatSnackBar,private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    let component = this.route.snapshot.component;
+    if (component == TrashComponent) {
+      this.isTrash = true;
+    }
+    if (component == ArchiveComponent) {
+      this.isArcheive = true;
+    }
   }
   trash(){
     let data={
@@ -45,6 +60,32 @@ colors:Array<any> = [
      console.log(response);
      this.displaytoIcons.emit(response);
     })
+    this.snackBar.open("note moved to trash",'',{duration:3000});
+  }
+  Restore() {
+    let data={
+      noteId:[this.notesCard.noteId],
+      //isDelete:true,
+    }
+    console.log(data);
+    this.notes.TrashNote(data).subscribe((response:any)=>{
+     console.log(response);
+     this.displaytoIcons.emit(response);
+    })
+    this.snackBar.open("Note Restored",'',{duration:3000});
+  }
+  Delete() {
+    let data={
+      noteId:[this.notesCard.noteId],
+    }
+    console.log(data);
+    this.notes.DeleteNote(data).subscribe((response: any) => {
+      console.log("Note Deleted Successfully",response);
+        console.log(response)
+     this.displaytoIcons.emit(response);
+
+    })
+    this.snackBar.open("Note Deleted Permanently",'',{duration:3000});
   }
   archeive(){
     let data={
@@ -55,7 +96,20 @@ colors:Array<any> = [
       console.log(response);
       this.displaytoIcons.emit(response);
     })
+    this.snackBar.open("Note moved to archive",'',{duration:3000});
   }
+  unArcheive(){
+    let data={
+      noteId:[this.notesCard.noteId],
+    }
+    console.log(data);
+    this.notes.ArchiveNote(data).subscribe((response:any)=>{
+      console.log(response);
+      this.displaytoIcons.emit(response);
+    })
+    this.snackBar.open("Note moved to Notes",'',{duration:3000});
+  }
+
   backColor(color:any){
     this.notesCard.color=color;
     let data={
@@ -68,5 +122,7 @@ colors:Array<any> = [
     console.log(response);
     console.log(this.notesCard);
     })
+    this.snackBar.open("Background color changed to "+color,'',{duration:3000});
+
   }
 }
